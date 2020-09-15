739be58ea4e94d659687562b6bb323c42e4e2b00
Rasswanth <rasswanths@elysiumit.com>
2020-09-15 14:07:44 +0530

    hid the firebase credentials

--- src/firebase.js
+++ src/firebase.js
@@ -2,13 +2,13 @@ import firebase from "firebase/app";
 import "firebase/firestore";
 
 var firebaseConfig = {
-	apiKey: "AIzaSyAE82PcKoFZKwEajRJwnFXCJtcx14ca5w0",
-	authDomain: "doodlebluetask.firebaseapp.com",
-	databaseURL: "https://doodlebluetask.firebaseio.com",
-	projectId: "doodlebluetask",
-	storageBucket: "doodlebluetask.appspot.com",
-	messagingSenderId: "579291075287",
-	appId: "1:579291075287:web:2481fd155c90e6c0ca5692",
+	apiKey: "API_KEY",
+	authDomain: "Domain_name",
+	databaseURL: "DATABASE_URL",
+	projectId: "ID",
+	storageBucket: "STORAGE_URL",
+	messagingSenderId: "SENDER_ID",
+	appId: "APP_ID",
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
